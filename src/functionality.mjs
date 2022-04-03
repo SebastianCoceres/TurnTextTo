7a4817401d619d01e9ef6637c5707e7c310fdeb4
Sebastian Coceres <sebastian.coceres11@gmail.com>
2022-04-03 19:37:09 +0200

    minor changes

--- src/functionality.mjs
+++ src/functionality.mjs
@@ -1,59 +1,45 @@
-import * as variables from "./variables.mjs";
+import * as v from "./variables.mjs";
 import { TextFormatter } from "./TextFormatter.mjs";
 
 //functionality
 const TextFormatter1 = new TextFormatter();
 
 export function updateData() {
-  TextFormatter1.inputValue = variables.inFormatter.value;
+  TextFormatter1.inputValue = v.inFormatter.value;
   let result;
 
-  if (variables.clearAccents.checked) {
-    result = TextFormatter1.removeAccents();
-  }
-  if (variables.toUpperCase.checked) {
-    result = TextFormatter1.toUpperCase();
-  }
-  if (variables.toLowerCase.checked) {
-    result = TextFormatter1.toLowerCase();
-  }
-  if (variables.capitalize.checked) {
-    result = TextFormatter1.capitalize();
-  }
-  if (variables.toCamelCase.checked) {
-    result = TextFormatter1.camelSentence();
-  }
+  if (v.clearAccents.checked) result = TextFormatter1.removeAccents();
+  if (v.toUpperCase.checked) result = TextFormatter1.toUpperCase();
+  if (v.toLowerCase.checked) result = TextFormatter1.toLowerCase();
+  if (v.capitalize.checked) result = TextFormatter1.capitalize();
+  if (v.toCamelCase.checked) result = TextFormatter1.camelSentence();
+  if (v.toPascalCase.checked) result = TextFormatter1.toPascalCase();
+  if (v.toSnakeCase.checked) result = TextFormatter1.toSnakeCase();
 
-  if (variables.toPascalCase.checked) {
-    result = TextFormatter1.toPascalCase();
-  }
-  if (variables.toSnakeCase.checked) {
-    result = TextFormatter1.toSnakeCase();
-  }
-
-  variables.outFormatter.innerHTML = result.inputValue;
+  v.outFormatter.innerHTML = result.inputValue;
 }
 
 const copyBtn = document.querySelector("#copyBtn");
 copyBtn.addEventListener("click", copy);
 
 async function copy() {
-  let copyText = variables.outFormatter;
+  //since execCommand() is deprecated is better do this instead
+  let copyText = v.outFormatter;
   var tempInput = document.createElement("input");
   tempInput.value = copyText.textContent;
   document.body.appendChild(tempInput);
   tempInput.select();
-  tempInput.setSelectionRange(0, 99999);
+  tempInput.setSelectionRange(0, 99999); //for mobile devices
   document.body.removeChild(tempInput);
   await navigator.clipboard.writeText(tempInput.value);
 
-  //
+  //just an animation
   copyBtn.classList.remove("btn-primary");
   copyBtn.classList.add("btn-success");
-  copyBtn.innerHTML = "copied!"
+  copyBtn.innerHTML = "copied!";
   setTimeout(() => {
     copyBtn.classList.remove("btn-success");
     copyBtn.classList.add("btn-primary");
-    copyBtn.innerHTML = "copy"
+    copyBtn.innerHTML = "copy";
   }, 2000);
 }
